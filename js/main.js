1e6b538185a8e88328104b19e86eb30c7c634580
@dgoncharenko <dgoncharenko@crispwp.com>
2019-06-17 19:22:59 +0300

    Применение эффекта для изображения

--- js/main.js
+++ js/main.js
@@ -83,7 +83,8 @@ var openUploadPopup = function () {
 var closeUploadPopup = function () {
   uploadPopup.classList.add('hidden');
   document.removeEventListener('keydown', onUploadEscPress);
-  uploadButtonClose.value = '';
+  inputUploadImage.value = '';
+  uploadImg.style = '';
 };
 
 var onUploadEscPress = function (evt) {
@@ -98,4 +99,71 @@ inputUploadImage.addEventListener('change', function () {
 
 uploadButtonClose.addEventListener('click', function () {
   closeUploadPopup();
+});
+
+var effectLevel = uploadPopup.querySelector('.effect-level');
+var effectPin = uploadPopup.querySelector('.effect-level__pin');
+var effectBar = uploadPopup.querySelector('.effect-level__depth');
+var effectRadio = uploadPopup.querySelectorAll('.effects__radio');
+var uploadImg = uploadPopup.querySelector('.img-upload__preview img');
+var inputEffectValue = uploadPopup.querySelector('input[name="effect-level"]');
+
+var calculateEffect = function (effectName, percent) {
+  var value;
+  var effect = '' + effectName;
+
+  if (effect === 'chrome' || effect === 'sepia') {
+    value = percent / 100;
+  } else if (effect === 'marvin') {
+    value = percent + '%';
+  } else if (effect === 'phobos') {
+    value = (percent * 3) / 100 + 'px';
+  } else if (effect === 'heat') {
+    value = (percent * 3) / 100;
+  }
+
+  return value;
+};
+
+effectLevel.classList.add('hidden');
+
+effectRadio.forEach(function (radioItem) {
+  radioItem.addEventListener('click', function () {
+    var radioValue = radioItem.value;
+    uploadImg.className = '';
+    uploadImg.style.filter = '';
+    inputEffectValue.value = '0';
+
+    if (radioValue !== 'none') {
+      uploadImg.classList.add('effects__preview--' + radioValue);
+      effectPin.style.left = '100%';
+      effectBar.style.width = '100%';
+      inputEffectValue.value = '100';
+    }
+
+    if (uploadImg.className === '') {
+      effectLevel.classList.add('hidden');
+    } else {
+      effectLevel.classList.remove('hidden');
+    }
+  });
+});
+
+effectPin.addEventListener('mouseup', function () {
+  var currentEffect = uploadPopup.querySelector('.effects__radio:checked').value;
+  var valuePin = parseInt(effectPin.style.left, 10);
+  var valueEffect = calculateEffect(currentEffect, valuePin);
+  inputEffectValue.value = valuePin;
+
+  if (currentEffect === 'chrome') {
+    uploadImg.style.filter = 'grayscale' + '(' + valueEffect + ')';
+  } else if (currentEffect === 'sepia') {
+    uploadImg.style.filter = 'sepia' + '(' + valueEffect + ')';
+  } else if (currentEffect === 'marvin') {
+    uploadImg.style.filter = 'invert' + '(' + valueEffect + ')';
+  } else if (currentEffect === 'phobos') {
+    uploadImg.style.filter = 'blur' + '(' + valueEffect + ')';
+  } else if (currentEffect === 'heat') {
+    uploadImg.style.filter = 'brightness' + '(' + valueEffect + ')';
+  }
 });
