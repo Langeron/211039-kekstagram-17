ffba503a0c4042b5f8fbfbf52967a4db7232fee5
@dgoncharenko <dgoncharenko@crispwp.com>
2019-06-18 21:42:18 +0300

    Правки и улучшения

--- js/main.js
+++ js/main.js
@@ -15,10 +15,6 @@ var NAMES = [
   'Алая Ведьма'
 ];
 
-var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-var pictureList = document.querySelector('.pictures');
-var fragment = document.createDocumentFragment();
-
 var PATH = {
   PHOTO: 'photos/',
   AVATAR: 'img/avatar-'
@@ -28,6 +24,20 @@ var FORMAT_IMG = {
   SVG: '.svg',
   JPG: '.jpg'
 };
+
+var KEY_ESC = 27;
+
+var SCALE = {
+  STEP: 25,
+  MIN: 25,
+  MAX: 100
+};
+
+var PHOTOS_COUNT = 25;
+
+var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+var pictureList = document.querySelector('.pictures');
+var fragment = document.createDocumentFragment();
 
 var getRandomNumber = function (min, max) {
   return Math.round(Math.random() * (max - min)) + min;
@@ -54,7 +64,7 @@ var getArrayPhotos = function (count) {
   return photos;
 };
 
-var photos = getArrayPhotos(25);
+var photos = getArrayPhotos(PHOTOS_COUNT);
 
 var renderPhoto = function (photo) {
   var pictureElement = pictureTemplate.cloneNode(true);
@@ -73,11 +83,14 @@ pictureList.appendChild(fragment);
 var uploadPopup = document.querySelector('.img-upload__overlay');
 var inputUploadImage = document.querySelector('.img-upload__input');
 var uploadButtonClose = uploadPopup.querySelector('.img-upload__cancel');
-var KEY_ESC = 27;
+var uploadImg = uploadPopup.querySelector('.img-upload__preview img');
+var inputEffectValue = uploadPopup.querySelector('input[name="effect-level"]');
 
 var openUploadPopup = function () {
   uploadPopup.classList.remove('hidden');
   document.addEventListener('keydown', onUploadEscPress);
+  uploadImg.style.transform = 'scale(1)';
+  inputScale.value = '100%';
 };
 
 var closeUploadPopup = function () {
@@ -93,33 +106,48 @@ var onUploadEscPress = function (evt) {
   }
 };
 
-inputUploadImage.addEventListener('change', function () {
-  openUploadPopup();
-});
+inputUploadImage.addEventListener('change', openUploadPopup);
 
-uploadButtonClose.addEventListener('click', function () {
-  closeUploadPopup();
-});
+uploadButtonClose.addEventListener('click', closeUploadPopup);
 
 var effectLevel = uploadPopup.querySelector('.effect-level');
 var effectPin = uploadPopup.querySelector('.effect-level__pin');
 var effectBar = uploadPopup.querySelector('.effect-level__depth');
 var effectRadio = uploadPopup.querySelectorAll('.effects__radio');
-var uploadImg = uploadPopup.querySelector('.img-upload__preview img');
-var inputEffectValue = uploadPopup.querySelector('input[name="effect-level"]');
+
+var Effect = {
+  CHROME: 'chrome',
+  SEPIA: 'sepia',
+  MARVIN: 'marvin',
+  PHOBOS: 'phobos',
+  HEAT: 'heat',
+  NONE: 'none'
+};
 
 var calculateEffect = function (effectName, percent) {
   var value;
-  var effect = '' + effectName;
+  var effect = effectName.toString();
 
-  if (effect === 'chrome' || effect === 'sepia') {
-    value = percent / 100;
-  } else if (effect === 'marvin') {
-    value = percent + '%';
-  } else if (effect === 'phobos') {
-    value = (percent * 3) / 100 + 'px';
-  } else if (effect === 'heat') {
-    value = (percent * 3) / 100;
+  switch (effect) {
+    case Effect.CHROME:
+      value = percent / 100;
+      break;
+
+    case Effect.SEPIA:
+      value = percent / 100;
+      break;
+
+    case Effect.MARVIN:
+      value = percent + '%';
+      break;
+
+    case Effect.PHOBOS:
+      value = (percent * 3) / 100 + 'px';
+      break;
+
+    case Effect.HEAT:
+      value = (percent * 3) / 100;
+      break;
   }
 
   return value;
@@ -127,80 +155,103 @@ var calculateEffect = function (effectName, percent) {
 
 effectLevel.classList.add('hidden');
 
+var addFilterEffect = function (evt) {
+  var radioItem = evt.target;
+  var radioValue = radioItem.value;
+  uploadImg.className = '';
+  uploadImg.style.filter = '';
+  inputEffectValue.value = '0';
+
+  if (radioValue !== Effect.NONE) {
+    uploadImg.classList.add('effects__preview--' + radioValue);
+    effectPin.style.left = '100%';
+    effectBar.style.width = '100%';
+    inputEffectValue.value = '100';
+  }
+
+  if (uploadImg.className === '') {
+    effectLevel.classList.add('hidden');
+  } else {
+    effectLevel.classList.remove('hidden');
+  }
+};
+
 effectRadio.forEach(function (radioItem) {
-  radioItem.addEventListener('click', function () {
-    var radioValue = radioItem.value;
-    uploadImg.className = '';
-    uploadImg.style.filter = '';
-    inputEffectValue.value = '0';
-
-    if (radioValue !== 'none') {
-      uploadImg.classList.add('effects__preview--' + radioValue);
-      effectPin.style.left = '100%';
-      effectBar.style.width = '100%';
-      inputEffectValue.value = '100';
-    }
-
-    if (uploadImg.className === '') {
-      effectLevel.classList.add('hidden');
-    } else {
-      effectLevel.classList.remove('hidden');
-    }
-  });
+  radioItem.addEventListener('click', addFilterEffect);
 });
 
+var FilterStyle = {
+  GRAYSCALE: 'grayscale',
+  SEPIA: 'sepia',
+  INVERT: 'invert',
+  BLUR: 'blur',
+  BRIGHTNESS: 'brightness'
+};
+
+var getFilterStyleCss = function (element, filter, valueEffect) {
+  element.style.filter = filter + '(' + valueEffect + ')';
+};
+
 effectPin.addEventListener('mouseup', function () {
-  var currentEffect = uploadPopup.querySelector('.effects__radio:checked').value;
+  var inputRadioChecked = uploadPopup.querySelector('.effects__radio:checked');
+  var currentEffect = inputRadioChecked.value;
   var valuePin = parseInt(effectPin.style.left, 10);
   var valueEffect = calculateEffect(currentEffect, valuePin);
   inputEffectValue.value = valuePin;
 
-  if (currentEffect === 'chrome') {
-    uploadImg.style.filter = 'grayscale' + '(' + valueEffect + ')';
-  } else if (currentEffect === 'sepia') {
-    uploadImg.style.filter = 'sepia' + '(' + valueEffect + ')';
-  } else if (currentEffect === 'marvin') {
-    uploadImg.style.filter = 'invert' + '(' + valueEffect + ')';
-  } else if (currentEffect === 'phobos') {
-    uploadImg.style.filter = 'blur' + '(' + valueEffect + ')';
-  } else if (currentEffect === 'heat') {
-    uploadImg.style.filter = 'brightness' + '(' + valueEffect + ')';
+  switch (currentEffect) {
+    case Effect.CHROME:
+      getFilterStyleCss(uploadImg, FilterStyle.GRAYSCALE, valueEffect);
+      break;
+
+    case Effect.SEPIA:
+      getFilterStyleCss(uploadImg, FilterStyle.SEPIA, valueEffect);
+      break;
+
+    case Effect.MARVIN:
+      getFilterStyleCss(uploadImg, FilterStyle.INVERT, valueEffect);
+      break;
+
+    case Effect.PHOBOS:
+      getFilterStyleCss(uploadImg, FilterStyle.BLUR, valueEffect);
+      break;
+
+    case Effect.HEAT:
+      getFilterStyleCss(uploadImg, FilterStyle.BRIGHTNESS, valueEffect);
+      break;
   }
 });
 
-var buttonScaleSmaller = document.querySelector('.scale__control--smaller');
-var buttonScaleBigger = document.querySelector('.scale__control--bigger');
-var inputScale = document.querySelector('.scale__control--value');
-var SCALE = {
-  STEP: 25,
-  MIN: 25,
-  MAX: 100
+var buttonScaleSmaller = uploadPopup.querySelector('.scale__control--smaller');
+var buttonScaleBigger = uploadPopup.querySelector('.scale__control--bigger');
+var inputScale = uploadPopup.querySelector('.scale__control--value');
+
+var getTransformStyleCss = function (element, scaleStyle) {
+  element.style.transform = 'scale' + '(' + scaleStyle + ')';
 };
 
-uploadImg.style.transform = 'scale(1)';
-inputScale.value = '100%';
-buttonScaleSmaller.addEventListener('click', function () {
+var scaleImage = function (evt) {
   var scaleNumber = parseInt(inputScale.value, 10);
-  if (scaleNumber > SCALE.MIN) {
-    scaleNumber = scaleNumber - SCALE.STEP;
-    if (scaleNumber < SCALE.MIN) {
-      scaleNumber = SCALE.MIN;
+  if (evt.target === buttonScaleSmaller) {
+    if (scaleNumber > SCALE.MIN) {
+      scaleNumber = scaleNumber - SCALE.STEP;
+      if (scaleNumber < SCALE.MIN) {
+        scaleNumber = SCALE.MIN;
+      }
     }
-    var scaleStyle = scaleNumber / 100;
-    inputScale.value = scaleNumber + '%';
-    uploadImg.style.transform = 'scale' + '(' + scaleStyle + ')';
+  } else {
+    if (scaleNumber < SCALE.MAX) {
+      scaleNumber = scaleNumber + SCALE.STEP;
+      if (scaleNumber > SCALE.MAX) {
+        scaleNumber = SCALE.MAX;
+      }
+    }
   }
-});
 
-buttonScaleBigger.addEventListener('click', function () {
-  var scaleNumber = parseInt(inputScale.value, 10);
-  if (scaleNumber < SCALE.MAX) {
-    scaleNumber = scaleNumber + SCALE.STEP;
-    if (scaleNumber > SCALE.MAX) {
-      scaleNumber = SCALE.MAX;
-    }
-    var scaleStyle = scaleNumber / 100;
-    inputScale.value = scaleNumber + '%';
-    uploadImg.style.transform = 'scale' + '(' + scaleStyle + ')';
-  }
-});
+  var scaleStyle = scaleNumber / 100;
+  inputScale.value = scaleNumber + '%';
+  getTransformStyleCss(uploadImg, scaleStyle);
+};
+
+buttonScaleSmaller.addEventListener('click', scaleImage);
+buttonScaleBigger.addEventListener('click', scaleImage);
