74d3111642930fd888581fcd0cea8df63165eb92
@dgoncharenko <dgoncharenko@crispwp.com>
2019-06-25 20:00:10 +0300

    Разделил form на модули и добавил загрузку фоток

--- js/render-photo.js
+++ js/render-photo.js
@@ -1,59 +1,11 @@
 'use strict';
 
 (function () {
-  var COMMENTS = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.'
-  ];
-
-  var NAMES = [
-    'Тор',
-    'Наташа',
-    'Халк',
-    'Ник Фьюри',
-    'Капитан Америка',
-    'Железный человек',
-    'Алая Ведьма'
-  ];
-
-  var PATH = {
-    PHOTO: 'photos/',
-    AVATAR: 'img/avatar-'
-  };
-
-  var FORMAT_IMG = {
-    SVG: '.svg',
-    JPG: '.jpg'
-  };
-
   var PHOTOS_COUNT = 25;
 
   var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var pictureList = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
-
-  var getArrayPhotos = function (count) {
-    var photos = [];
-    for (var i = 1; i <= count; i++) {
-      var photosOption = {
-        url: PATH.PHOTO + i + FORMAT_IMG.JPG,
-        likes: window.util.getRandomNumber(15, 200),
-        comments: [
-          {
-            avatar: PATH.AVATAR + window.util.getRandomNumber(1, 6) + FORMAT_IMG.SVG,
-            message: COMMENTS[window.util.getRandomNumber(0, COMMENTS.length - 1)],
-            name: NAMES[window.util.getRandomNumber(0, NAMES.length - 1)]
-          }
-        ]
-      };
-
-      photos.push(photosOption);
-    }
-
-    return photos;
-  };
-
-  var photos = getArrayPhotos(PHOTOS_COUNT);
 
   var renderPhoto = function (photo) {
     var pictureElement = pictureTemplate.cloneNode(true);
@@ -63,10 +15,13 @@
     fragment.appendChild(pictureElement);
   };
 
-  for (var i = 0; i < photos.length; i++) {
-    renderPhoto(photos[i]);
-  }
+  var onSuccess = function (photos) {
+    for (var i = 0; i < PHOTOS_COUNT; i++) {
+      renderPhoto(photos[i]);
+    }
 
-  pictureList.appendChild(fragment);
+    pictureList.appendChild(fragment);
+  };
 
+  window.load(onSuccess);
 })();
