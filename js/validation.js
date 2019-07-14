a694c02c4a7297f12a7fbe4ebc0805143186aee9
@dgoncharenko <dgoncharenko@crispwp.com>
2019-07-14 13:21:51 +0300

    Доработал валидацию

--- js/validation.js
+++ js/validation.js
@@ -5,48 +5,55 @@
   var SPACE = ' ';
   var MAX_LENGTH = 20;
   var HASHTAGS_MAX_AMOUNT = 5;
+  var LATTICE = '#';
+  var ErrorInput = {
+    MIN_LENGTH: 'хеш-тег не может состоять только из одной решётки',
+    SEPARATED_BY_SPACES: 'хэш-теги разделяются пробелами',
+    MAX_LENGTH: 'максимальная длина одного хэш-тега 20 символов, включая решётку',
+    FIRST_SYMBOL: 'хэш-тег начинается с символа # (решётка)',
+    DOUBLE_HASHTAG: 'один и тот же хэш-тег не может быть использован дважды',
+    MAX_QUANTITY: 'нельзя указать больше пяти хэш-тегов'
+  };
 
   var form = document.querySelector('.img-upload__form');
   var inputHashtag = form.querySelector('.text__hashtags');
 
   var checkHashtagInput = function (input) {
     var inputValue = input.value;
+    inputHashtag.setCustomValidity('');
 
-    var hashtags = inputValue.split(SPACE).filter(function (item) {
+    var hashtags = inputValue.toLowerCase().split(SPACE).filter(function (item) {
       return item !== '';
     });
 
+
     hashtags.forEach(function (hashtag, i) {
-      if (hashtag[0] === '#') {
+      if (hashtag[0] === LATTICE) {
         if (hashtag.length === 1) {
-          input.setCustomValidity('хеш-тег не может состоять только из одной решётки');
+          input.setCustomValidity(ErrorInput.MIN_LENGTH);
         }
-        if (hashtag.indexOf('#', 1) !== -1) {
-          input.setCustomValidity('хэш-теги разделяются пробелами');
+        if (hashtag.indexOf(LATTICE, 1) !== -1) {
+          input.setCustomValidity(ErrorInput.SEPARATED_BY_SPACES);
         }
-        if (hashtag.lenght > MAX_LENGTH) {
-          input.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку');
+        if (hashtag.length > MAX_LENGTH) {
+          input.setCustomValidity(ErrorInput.MAX_LENGTH);
         }
 
       } else {
-        input.setCustomValidity('хэш-тег начинается с символа # (решётка)');
+        input.setCustomValidity(ErrorInput.FIRST_SYMBOL);
       }
 
-      if (hashtags.indexOf(hashtag, i + 1) !== -1) {
-        input.setCustomValidity('один и тот же хэш-тег не может быть использован дважды');
+      if (hashtags.includes(hashtag, i + 1)) {
+        input.setCustomValidity(ErrorInput.DOUBLE_HASHTAG);
       }
     });
 
     if (hashtags.length > HASHTAGS_MAX_AMOUNT) {
-      input.setCustomValidity('нельзя указать больше пяти хэш-тегов');
+      input.setCustomValidity(ErrorInput.MAX_QUANTITY);
     }
   };
 
-  form.addEventListener('submit', function (evt) {
-    evt.preventDefault();
+  inputHashtag.addEventListener('blur', function () {
     checkHashtagInput(inputHashtag);
-    if (!inputHashtag.checkValidity()) {
-      evt.preventDefault();
-    }
   });
 })();
